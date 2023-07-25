8eea15cfc9b22eb1f42fdbcdd0a442ffcf9a21eb
Matias Falcone <matias@greasemonkeygames.com>
2023-07-25 13:57:49 +1000

    completed chapter 3

--- pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -37,11 +37,42 @@
 const course = useCourse();
 const route = useRoute();
 
+definePageMeta({
+  validate({ params }) {
+
+    const course = useCourse();
+
+    const chapter = course.chapters.find(
+      (chapter) => chapter.slug === params.chapterSlug
+    )
+    if(!chapter.value) {
+      throw createError({
+        statusCode: 404,
+        message: "Chapter not found"
+      })
+    }
+
+    const lesson = chapter.lessons.find(
+      (lesson) => lesson.slug === params.lessonSlug
+    )
+    if(!lesson.value) {
+      throw createError({
+        statusCode: 404,
+        message: "Lesson not found"
+      })
+    }
+
+    return true;
+  }
+})
+
 const chapter = computed(() => {
   return course.chapters.find(
     (chapter) => chapter.slug === route.params.chapterSlug
   );
 });
+
+
 
 const lesson = computed(() => {
   return chapter.value.lessons.find(
