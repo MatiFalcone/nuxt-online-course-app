0e60eabe7834fc42ead6fe7347553e96586a1d21
Matias Falcone <matias@greasemonkeygames.com>
2023-08-10 18:56:22 +1000

    fixed problem with types

--- server/api/course/meta.get.ts
+++ server/api/course/meta.get.ts
@@ -21,7 +21,10 @@ const chapterSelect = Prisma.validator<Prisma.ChapterDefaultArgs>()({
   },
 });
 
-export type ChapterOutline = Prisma.ChapterGetPayload<typeof chapterSelect>;
+export type ChapterOutline = Omit<
+  Prisma.ChapterGetPayload<typeof chapterSelect>,
+  "lessons"
+> & { lessons: LessonOutline[] };
 
 const courseSelect = Prisma.validator<Prisma.CourseDefaultArgs>()({
   select: {
@@ -30,8 +33,33 @@ const courseSelect = Prisma.validator<Prisma.CourseDefaultArgs>()({
   },
 });
 
-export type CourseOutline = Prisma.CourseGetPayload<typeof courseSelect>;
+export type CourseOutline = Omit<
+  Prisma.CourseGetPayload<typeof courseSelect>,
+  "chapters"
+> & {
+  chapters: ChapterOutline[];
+};
 
-export default defineEventHandler((event) => {
-  return prisma.course.findFirst(courseSelect);
+export default defineEventHandler(async (): Promise<CourseOutline> => {
+  const outline = await prisma.course.findFirst(courseSelect);
+
+  if (!outline) {
+    throw createError({
+      statusCode: 404,
+      statusMessage: "Course not found",
+    });
+  }
+
+  const chapters = outline.chapters.map((chapter) => ({
+    ...chapter,
+    lessons: chapter.lessons.map((lesson) => ({
+      ...lesson,
+      path: `/course/chapter/${chapter.slug}/lesson/${lesson.slug}`,
+    })),
+  }));
+
+  return {
+    ...outline,
+    chapters,
+  };
 });
